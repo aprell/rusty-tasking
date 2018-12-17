8d98da83f93810fbed87abc800ce98dde70c5b00
Andreas Prell <andreas.h.prell@gmail.com>
2018-12-17 19:58:47 +0100

    Sketch tasks

---
+++ src/task.rs
@@ -0,0 +1,86 @@
+// Storing closures requires generics and trait bounds. All closures implement
+// at least one of the traits `Fn`, `FnMut`, or `FnOnce`. For instance, a
+// closure that implements `FnMut` may capture variables by reference or
+// mutable reference.
+
+// From TRPL: "[...] we need `Send` to transfer the closure from one thread to
+// another and `'static` (a lifetime bound) because we don’t know how long the
+// thread will take to execute."
+pub type Thunk<T> = FnMut() -> T + Send + 'static;
+
+// `Send` is a supertrait of `Task`, which means that only those task types
+// that can be sent between threads safely are allowed to implement `Task`.
+pub trait Task: Send {
+    fn run(self: Box<Self>);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // From TRPL: "The golden rule of dynamically sized types is that we must
+    // always put values of dynamically sized types behind a pointer of some
+    // kind."
+    struct SimpleTask<T>(Box<Thunk<T>>);
+
+    impl<T> SimpleTask<T> {
+        fn new(task: Box<Thunk<T>>) -> SimpleTask<T> {
+            SimpleTask(task)
+        }
+    }
+
+    impl<T> Task for SimpleTask<T> {
+        fn run(mut self: Box<SimpleTask<T>>) {
+            // Ignore result
+            let _ = (*self).0();
+        }
+    }
+
+    #[test]
+    fn simple_task() {
+        // Unboxed task + boxed closure
+        let mut a = SimpleTask::new(Box::new(|| 1));
+        a.0();
+
+        // Boxed task + boxed closure
+        let a = Box::new(SimpleTask::new(Box::new(|| 1)));
+        a.run();
+        // `a` has been consumed
+    }
+
+    #[test]
+    fn simple_unboxed_task_collection() {
+        // Three tasks with different types
+        let a = SimpleTask::new(Box::new(|| ()));
+        let b = SimpleTask::new(Box::new(|| 1));
+        let c = SimpleTask::new(Box::new(|| 1.2));
+
+        // A vector of unboxed trait objects
+        let mut v: Vec<&Task> = vec![&a, &b, &c];
+
+        while let Some(_t) = v.pop() {
+            // `_t` has type `&Task`
+            // Can we do something useful with `_t`?
+        }
+    }
+
+    #[test]
+    fn simple_boxed_task_collection() {
+        // Three tasks with different types
+        let a = SimpleTask::new(Box::new(|| ()));
+        let b = SimpleTask::new(Box::new(|| 1));
+        let c = SimpleTask::new(Box::new(|| 1.2));
+
+        // A vector of boxed trait objects
+        let mut v: Vec<Box<Task>> = vec![
+            Box::new(a),
+            Box::new(b),
+            Box::new(c),
+        ];
+
+        while let Some(t) = v.pop() {
+            // `t` has type `Box<Task>`, which can be moved into `run`
+            t.run();
+        }
+    }
+}
