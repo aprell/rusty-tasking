d5db85c19140a94f31921b28ace8f6674109c0e6
Andreas Prell <andreas.h.prell@gmail.com>
2019-02-08 15:19:14 +0100

    Add two tests

--- src/macros.rs
+++ src/macros.rs
@@ -25,3 +25,62 @@ macro_rules! async_future {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::runtime::Runtime;
+    use crate::task::Async;
+    use crate::worker::Worker;
+
+    #[test]
+    fn async_tasks() {
+        let runtime = Runtime::init(3);
+        let master = runtime.master;
+
+        for _ in 0..5 {
+            async_task! {
+                for _ in 0..5 {
+                    async_task! {
+                        for _ in 0..3 {
+                            async_task!();
+                        }
+                    }
+                }
+            }
+        }
+
+        let mut num_tasks_executed = 0;
+        while master.has_tasks() {
+            master.try_handle_steal_request();
+            match master.pop() {
+                Some(task) => {
+                    task.run();
+                    num_tasks_executed += 1;
+                }
+                None => break,
+            }
+        }
+
+        master.stats.num_tasks_executed.increment(num_tasks_executed);
+
+        // TODO Task barrier needed
+
+        let stats = runtime.join();
+        assert_eq!(stats.num_tasks_executed.get(), 105);
+    }
+
+    fn sum(n: u32) -> u32 {
+        if n <= 1 { n } else { n + async_future!(sum(n - 1)).wait() }
+    }
+
+    #[test]
+    fn async_futures() {
+        let runtime = Runtime::init(3);
+
+        let n = async_future!(sum(10));
+        assert_eq!(n.wait(), 55);
+
+        let stats = runtime.join();
+        assert_eq!(stats.num_tasks_executed.get(), 10);
+    }
+}
